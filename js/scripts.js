e8b4a19ea92d7ab75ea857c06545b148e6a50f02
marcinprusko <marcinprusko@gmail.com>
2017-03-06 21:23:01 +0100

    DOM

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+﻿// plik scripts.js
+
+var buttonjs = document.getElementsByClassName('button');
+console.log(buttonjs);
+
+var listsElement = document.getElementsByTagName('<>');
+for (var i = 0; i < listsElement.length; i++) 
+{
+	function buttons() 
+	{
+	alert(buttonjs.innertext);
+	}
+console.log(alert);
+}
+
+buttons
